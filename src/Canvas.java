0bb7cd2475261878d843e70610d77682f94797bf
Nova Casserfelt <casserfelt.nova@elev.ga.ntig.se>
2023-02-15 15:15:18 +0100

    hej

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class Canvas {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Canvas extends JPanel implements MouseListener{
+
+    String c1 = "X";
+    String c2 = "O";
+    boolean color1 = false;
+
+    public Canvas(){
+
+        this.setBackground(Color.pink);
+        addMouseListener((MouseListener) this);
+        this.setPreferredSize(new Dimension(300, 300));
+
+    }
+
+    public void X(Graphics g){
+        for (MouseListener =)
+        int posx = 50;
+        int posy = 50;
+        String X = "X";
+        g.drawString(X, posx, posy);
+
+    }
+
+    public void O(){
+
+    }
+
+    @Override
+    public void paint(Graphics g){
+        super.paintComponent(g);
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        System.out.println("CLICK!");
+        System.out.println(e.getButton());
+        if (e.getButton() == 1) {
+
+            if (color1) {
+                this.Set;
+            } else {
+                this.setToolTipText(c2);
+            }
+            color1 = !color1;
+        }else{
+            this.setBackground(Color.blue);
+        }
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+        System.out.println("Pressed");
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
 }
--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        this.setTitle("Fun with layouts");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setLayout(new GridLayout(3, 3));
+        for(int i = 0; i<3*3; i++){
+            this.add(new Canvas());
+        }
+
+        this.pack();
+        this.setVisible(true);
+    }
+
 }
